0a090819e84048e10f0d00d4776c95b9c98efe4a
Swan <swan.marin@epitech.eu>
2024-02-11 17:24:09 +0100

    Mise en place de react

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+/* Home page */
+function Home () {
+
+
+    return (
+        <div className='homepage'>
+            <main>
+                <section className="features">
+                </section>
+            </main>
+        </div>
+    )
+}
+
+export default Home
