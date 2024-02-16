0937fd8e7f58797314511ac84b68fcee51c0a31d
Swan <swan.marin@epitech.eu>
2024-02-16 05:23:12 +0100

    omepage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../src/pages/Home/Home.jsx';
 import Login from '../src/pages/Login/Login.jsx';
-import '../src/sass/_Main.scss';
+import '../src/sass/_main.scss';
 
 export default function App () {
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,34 @@
-/* Home page */
-function Home () {
+import React from 'react';
+import Banner from '../../components/Banner.jsx';
+import Item from '../../components/Item.jsx';
+import FeaturesItemData from '../../data/ItemData.json';
+import iconChat from '../../assets/icon-chat.png';
+import iconMoney from '../../assets/icon-money.png';
+import iconSecurity from '../../assets/icon-security.png';
+import '../../sass/pages/_home.scss';
 
+function Home () {
+    const imageData = {
+        "icon-chat.webp": iconChat,
+        "icon-money.webp": iconMoney,
+        "icon-security.webp": iconSecurity
+    }
 
     return (
         <div className='homepage'>
             <main>
+                <Banner />
                 <section className="features">
+                    <h2 className='sr-only'>Features</h2>
+                    {FeaturesItemData.map((data) => (
+                        < Item 
+                            key={data.id}
+                            image={imageData[data.image]}
+                            descriptionImage={data.descriptionImage}
+                            title={data.title}
+                            description={data.description}
+                        />
+                    ))}
                 </section>
             </main>
         </div>
