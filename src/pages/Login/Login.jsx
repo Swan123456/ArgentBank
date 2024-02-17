5050a798daea21f6bf244d28d5a14643fd97fd4f
Swan <swan.marin@epitech.eu>
2024-02-17 15:39:55 +0100

    squelette + style LoginPage

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Form from '../../components/Form';
+import '../../sass/pages/_signIn.scss';
+
+/* Login page */
+function Login () {
+    return (
+        <div className='signin-page'>
+            <main className='bg-dark'>
+                < Form />
+            </main>
+        </div>
+        
+    )
+}
+
+export default Login;
