40dacac3dbf1733c96aed89898cc44e23f8400a4
Swan <swan.marin@epitech.eu>
2024-02-28 18:22:29 +0100

    Error page

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import '../src/sass/_main.scss';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Profile from './pages/Profile/Profile.jsx';
+import Error from './pages/Error/Error.jsx'
 
 export default function App () {
 
@@ -16,6 +17,7 @@ export default function App () {
                 <Route path='/' element={<Home />} />
                 <Route path='login' element={<Login />} />
                 <Route path='profile' element={<Profile />} />
+                <Route path="*" element={<Error />} />
             </Routes>
             <Footer />
         </div>
